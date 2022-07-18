ff9771cb4aad5cc5f0bde5004080528406a40eea
manotti22 <ottizoubairi@gmail.com>
2022-07-19 00:31:12 +0200

    logger and exception files completed

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask 
+import sys
+from banking.logger import logging
+from banking.exception import BankingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','post'])
 def index():
-    return 'CD CI PIPLINE IS STARTED'
+
+    try:
+        raise Exception('WE ARE TESTING CUSTUM EXCEPTION')
+    except Exception as e:
+        banking= BankingException(e,sys)
+        logging.info(banking.error_message)
+    logging.info('we are testing logging modul')
+    return 'CD CI PIPLINE has been established'
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ banking/logger/__init__.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import logging
+import os
+from datetime import datetime
+
+LOG_DIR= "banking_logs"
+
+CURRENT_TIME_STEMP= f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+log_file_name= f"log{CURRENT_TIME_STEMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_file_path= os.path.join(LOG_DIR,log_file_name)
+
+logging.basicConfig(filename=log_file_path,
+filemode="w",
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
+)
