542d31be522d4b44d98f2890b92c68bccca1e22b
manotti22 <ottizoubairi@gmail.com>
2022-07-19 02:01:42 +0200

    updated

---
+++ banking/config/configuration.py
@@ -0,0 +1,16 @@
+
+
+class Configuration:
+
+    def __init__(self) -> None:
+        pass
+
+    def get_data_ingestion_config(self):
+        pass
+
+    def get_data_validation_config(self):
+        pass
+
+    def get_data_transformation_config(self):
+        pass
+
