5e5a1c0efc22cd4a0425ac2ac170c8c0c9320c7a
manotti22 <ottizoubairi@gmail.com>
2022-07-19 16:48:15 +0200

    updated

---
+++ banking/pipeline/pipeline.py
@@ -0,0 +1,36 @@
+
+from banking.config.configuration import Configuration
+from banking.logger import logging
+from banking.exception import BankingException
+from banking.entity.artifact_entity import ModelPusherArtifact, DataIngestionArtifact, ModelEvaluationArtifact
+from banking.entity.config_entity import DataIngestionConfig
+from banking.component.data_ingestion import DataIngestion
+
+import os, sys
+
+
+
+
+class Pipeline:
+   
+    def __init__(self, config: Configuration ) -> None:
+        try:
+            self.config = config
+        except Exception as e:
+            raise BankingException(e, sys) from e
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
+            return data_ingestion.initiate_data_ingestion()
+        except Exception as e:
+            raise BankingException(e, sys) from e
+
+    
+    def run_pipeline(self):
+
+        try:
+
+           data_ingestion_artifact = self.start_data_ingestion()
+        except Exception as e:
+            raise BankingException(e, sys) from e
